c371a5562a0052d3cb72e0ae2af52ccb1385b732
Ferns <jeffreyzhang123@gmail.com>
2021-06-26 14:10:03 -0700

    STYLE and address output

--- CPU.sv
+++ CPU.sv
@@ -3,14 +3,14 @@
 `include "control_unit.sv"
 
 module CPU(
-	input wire i_clk,           // The clock signal
-	input wire [31:0] i_inst,   // The instruction to execute
-	input wire [31:0] i_mem,    // The data to be loaded from memory
-	output wire o_write,        // Whether to write to memory or not
-	output wire [31:0] o_pc,    // The next instruction to fetch
-	output wire [31:0] o_mem,   // The data to write to memory
-	output wire [31:0] o_addr,  // The memory address to write to
-	output reg  [1:0] o_memsize // The size of memory to be written
+	input wire i_clk,            // The clock signal
+	input wire [31:0] i_inst,    // The instruction to execute
+	input wire [31:0] i_mem,     // The data to be loaded from memory
+	output wire o_write,         // Whether to write to memory or not
+	output wire [31:0] o_pc,     // The next instruction to fetch
+	output wire [31:0] o_mem,    // The data to write to memory
+	output wire [31:0] o_addr,   // The memory address to write to or read from
+	output reg  [1:0]  o_memsize // The size of memory to be written
 );
 
 	reg [31:0] pc; // Program counter register, keeps track of current instruction
@@ -43,6 +43,7 @@ module CPU(
 	wire a_ysel;       // Source of the second operand, immediate or register
 	wire a_zero;       // Whether the result of the ALU op was 0 or not
 	assign a_x = r_rdata1;
+	assign o_addr = a_res;
 	ALU alu(a_op, a_op2, a_x, a_y, a_res, a_zero);
 
 	always_comb begin
