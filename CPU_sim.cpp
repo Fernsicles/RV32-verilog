7486ae5477aedf31eafba35a5c5a717d35b5fb5a
Ferns <jeffreyzhang123@gmail.com>
2021-06-27 14:26:49 -0700

    Fancier simulator and some programs

--- CPU_sim.cpp
+++ CPU_sim.cpp
@@ -1,34 +1,100 @@
 #include "obj_dir/VCPU.h"
 #include "verilated.h"
-#include <string>
-#include <iostream>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <filesystem>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	Verilated::commandArgs(argc, argv);
+
+	extern char *optarg;
+	extern int optind;
+	int c, err = 0;
+	int hflag = 0, pflag = 0, mflag = 0, vflag = 0;
+	char *pstring;
+	int memsize;
+	static char usage[] = "usage: h [-h] [-v] [-i program] [-m memory_size]";
+	while((c = getopt(argc, argv, "hp:m:v")) != -1) {
+		switch(c) {
+			case 'h':
+				hflag = 1;
+				break;
+			case 'p':
+				pflag = 1;
+				pstring = optarg;
+				break;
+			case 'm':
+				mflag = 1;
+				memsize = stoi(optarg, nullptr);
+				break;
+			case 'v':
+				vflag = 1;
+				break;
+			default:
+				cout << usage << endl;
+				abort();
+		}
+	}
+
+	uint *inst;
+	uint8_t *mem = (uint8_t *) calloc(memsize, sizeof(uint8_t));
+	ifstream file;
+	file.open(pstring, ios::in | ios::binary);
+	if(vflag) {
+		int i = 0;
+		while(!file.eof()) {
+			uint buf;
+			file.read((char *) &buf, 1);
+			mem[i] = buf;
+			i++;
+		}
+	} else {
+		int i = 0;
+		inst = (uint *) calloc(filesystem::file_size(pstring) / 4, sizeof(uint));
+		while(!file.eof()) {
+			int buf;
+			file.read((char *) &buf, 4);
+			inst[i] = buf;
+			i++;
+		}
+	}
+
+	if(vflag) {
+		for(int x = 0; x < filesystem::file_size(pstring) / 4; x++) {
+			cout << "0x";
+			cout << setw(8) << setfill('0') << setbase(16) << ((uint *) mem)[x] << endl;
+		}
+	} else {
+		for(int x = 0; x < filesystem::file_size(pstring) / 4; x++) {
+			cout << "0x";
+			cout << setw(8) << setfill('0') << setbase(16) << inst[x] << endl;
+		}
+	}
+	
+
+	Verilated::commandArgs(0, argv);
 	VCPU cpu;
 
-	unsigned int inst[] = {
-		0x04000293,
-		0x00528023,
-		0xfff28293,
-		0xfe029ce3,
-		0x000000ef
-	};
-
-	uint8_t mem[64];
-	fill(mem, mem + 64, 0);
-
 	cpu.i_clk = 0;
-	cpu.i_inst = inst[0];
+	if(vflag) {
+		cpu.i_inst = ((uint *) mem)[0];
+	} else {
+		cpu.i_inst = inst[0];
+	}
 	cpu.i_mem = mem[0];
 	cpu.eval();
 
-	for(int x = 0; x < 200; x++) {
+	while(cpu.i_inst != 0x0000006f) {
 		cpu.i_clk = 0;
-		cpu.i_inst = inst[cpu.o_pc >> 2];
+		if(vflag) {
+			cpu.i_inst = ((uint *) mem)[cpu.o_pc >> 2];
+		} else {
+			cpu.i_inst = inst[cpu.o_pc >> 2];
+		}
 		cpu.i_mem = cpu.o_addr;
 		cpu.eval();
 		cpu.i_clk = 1;
@@ -45,7 +111,8 @@ int main(int argc, char **argv) {
 				case 3:
 					mem[cpu.o_addr] = (uint8_t) cpu.o_mem;
 					mem[cpu.o_addr + 1] = (uint8_t) (cpu.o_mem >> 8);
-					mem[cpu.o_addr + 2] = (uint8_t) (cpu.o_mem >> 8);
+					mem[cpu.o_addr + 2] = (uint8_t) (cpu.o_mem >> 16);
+					mem[cpu.o_addr + 3] = (uint8_t) (cpu.o_mem >> 24);
 					break;
 				default:
 					break;
@@ -53,7 +120,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	for(int x = 0; x < 64; x++) {
+	for(int x = 0; x < memsize; x++) {
 		cout << "0x";
 		cout << setw(2) << setfill('0') << setbase(16) << (int) mem[x] << '\t';
 		if((x + 1) % 16 == 0) {
