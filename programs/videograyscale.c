f2b3e002b784e1c34f1bb5e6bab375cac8af0972
Kai Tamkun <ktamkun@ucsc.edu>
2021-08-05 01:53:13 -0700

    Add grayscale video test

---
+++ programs/videograyscale.c
@@ -0,0 +1,18 @@
+#include <string.h>
+void main() {
+	unsigned char *framebuffer = (unsigned char *) 0x80000000;
+	unsigned char *ibase = (unsigned char *) 0x100;
+	unsigned int *tbase = (unsigned int *) 0x0;
+	int i = 0;
+	unsigned int t = tbase[0];
+	unsigned int now;
+	for (unsigned int x = 0; x < 6572; x++) {
+		now = tbase[0];
+		while (now - t < 34)
+			now = tbase[0];
+		t = now;
+		memcpy(framebuffer, ibase, 360 * 480);
+		ibase += 360 * 480;
+	}
+	while(1);
+}
