708a84a5c8e8e4b71561deea0c3cd3ec1cd0c377
Kai Tamkun <ktamkun@ucsc.edu>
2021-08-05 05:34:56 -0700

    Update programs/image.c

--- programs/image.c
+++ programs/image.c
@@ -1,9 +1,10 @@
 __asm__("li sp, 0x10000000");
 // __asm__("a: jal zero, a");
 void main() {
-    unsigned char *rbase = (unsigned char *) 0x80000000;
-	unsigned char *gbase = (unsigned char *) 0x80000000 + 360 * 480;
-	unsigned char *bbase = (unsigned char *) 0x80000000 + 360 * 480 * 2;
+	unsigned char *framebuffer = (unsigned char *) 0x80100000;
+    unsigned char *rbase = framebuffer;
+	unsigned char *gbase = framebuffer + 360 * 480;
+	unsigned char *bbase = framebuffer + 360 * 480 * 2;
 	unsigned char *ibase = (unsigned char *) 0x100;
 	for(int x = 0; x < 480 * 360; x++) {
 		unsigned char pixel = ibase[x];
