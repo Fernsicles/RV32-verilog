bd798c9ba827b56c2f09d3a41622d85f1704fcd8
Kai Tamkun <ktamkun@ucsc.edu>
2021-08-09 03:13:20 -0700

    I solved the mystery: the first function is the first to be executed, not necessarily main

---
+++ programs/literal.c
@@ -0,0 +1,4 @@
+void main() {
+	const char *foo = "Hello!";
+	for (;;);
+}
--- programs/puts.c
+++ programs/puts.c
@@ -1,10 +1,12 @@
-void puts(const char *str) {
-	for (int i = 0; str[i] != '\0'; ++i)
-		*(volatile unsigned char *) 0x80000001 = str[i];
-}
+void puts(const char *str);
 
 void main() {
 	for (;;) {
-		puts("Hello, World!\n");
+		puts("Hello, World!\r\n");
 	}
+}
+
+void puts(const char *str) {
+	for (int i = 0; str[i] != '\0'; ++i)
+		*(volatile unsigned char *) 0x80000001 = str[i];
 }
